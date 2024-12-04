d8adc8752f18c12f9e402a48750f9c1f545e7a95
FreshPrinceofQueens <tayooduyemi1@gmail.com>
2024-12-03 21:20:19 -0500

    Creates setup script

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+REPO_NAME = "book_recommender"
+AUTHOR_USER_NAME = "FreshPrinceofQueens"
+SRC_REPO = "src"
+LIFT_OF_REQUIREMENTS = ['streamlit', 'numpy']
+
+
+setup(
+    name = SRC_REPO,
+    version = "0.0.1",
+    author = AUTHOR_USER_NAME,
+    author_email = "andrewjcarter@gmail.com",
+    description = ("A small package for a book recommender system"),
+    long_description= long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FreshPrinceofQueens/book_recommender",
+    author_email="tayooduyemi1@gmail.com",
+    packages=[SRC_REPO],
+    python_requires=">=3.7",
+    install_requires=LIFT_OF_REQUIREMENTS
+)
